6f132f595350c589c2a87982f185acb1545e9acd
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-01-25 22:30:44 +0530

    server now loading every file and image.
    created a public folder which contains all the static-html, images,js.
    
    sever loads every static image and file.

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const { Server } = require("net");
 const fs = require("fs");
+const STATIC_DIR = `${__dirname}/public`;
+
+const loadResponseText = function(url) {
+  console.log(url);
+  const [, fileExt] = url.split(".");
+  const data = fs.readFileSync(url);
+  const response = [
+    "http/1.1 200 OK",
+    `content-type: ${fileExt}`,
+    `content-length: ${data.length}`,
+    "",
+    ""
+  ];
+  return { response: response.join("\n"), data };
+};
+const getCompleteURl = function(url) {
+  let completeUrl = `${STATIC_DIR}${url}`;
+  if (url === "/") {
+    completeUrl = `${STATIC_DIR}/index.html`;
+  }
+  return completeUrl;
+};
 const generatePageResponse = function(data) {
   const [request] = data.split("\n");
   const [method, url] = request.split(" ");
-  if (method === "GET" && url === "/") {
-    const data = fs.readFileSync("index.html");
-    const response = [
-      "http/1.1 200 OK",
-      "content-type : text/html",
-      `content-length: ${data.length}`,
-      "",
-      ""
-    ];
-    return { response: response.join("\n"), data };
+  const completeUrl = getCompleteURl(url);
+  if (fs.existsSync(completeUrl)) {
+    return loadResponseText(completeUrl);
   }
   const response = [
     "http/1.1 404 NOT FOUND",
