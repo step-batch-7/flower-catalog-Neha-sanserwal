674c4ecd4f8cdc6411db4e2389e37f5666742eca
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-01-28 16:30:55 +0530

    deleted file request and response.
    refactored server and used http server instead of net

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
-const { Server } = require("net");
+const http = require("http");
 const fs = require("fs");
-const { Response } = require("./response");
-const { Request } = require("./request");
-const DEFAULT_RESPONSE = {
-  protocol: "HTTP/1.1",
-  status: "404",
-  statusMsg: "NOT FOUND"
-};
+const url = require("url");
+const STATIC_DIR = `${__dirname}/public`;
 
-const DEFAULT_HEADERS = {
-  "Content-Type": "text/html",
-  "Content-Length": "0"
+const getFileExtension = function(fileName) {
+  const [, fileExt] = fileName.split(".");
+  return fileExt;
 };
-const loadComments = function() {
+const loadOlderComments = function() {
   if (!fs.existsSync("./comments.json")) {
     fs.writeFileSync("./comments.json", JSON.stringify([]));
   }
   let comments = fs.readFileSync("./comments.json", "utf8");
   return JSON.parse(comments);
 };
-
-const saveComments = function(newRequest) {
-  newRequest.changeBody();
-  let comments = loadComments();
-  comments.unshift(newRequest.body);
-  fs.writeFileSync("./comments.json", JSON.stringify(comments));
+const parseCommentsDetails = function(comment) {
+  const resultedBody = url.parse(`?${comment}`, true).query;
+  resultedBody.date = new Date().toLocaleDateString();
+  resultedBody.time = new Date().toLocaleTimeString();
+  return resultedBody;
 };
+const saveComments = function(newRequest, newResponse) {
+  let comment = "";
+  newRequest.on("data", chunk => {
+    comment += chunk;
+  });
+
+  newRequest.on("end", () => {
+    let parsedComment = parseCommentsDetails(comment);
+    let comments = loadOlderComments();
+    comments.unshift(parsedComment);
+    fs.writeFileSync("./comments.json", JSON.stringify(comments));
+    serveGuestPage(newRequest, newResponse);
+  });
+};
+
 const readCommentList = function(comment) {
   let commentTemplate = fs.readFileSync("./commentTemplate.html", "utf8");
 
@@ -35,77 +44,53 @@ const readCommentList = function(comment) {
   return commentTemplate;
 };
 
-const serveGuestPage = function(newRequest, newResponse) {
-  let guestPage = fs.readFileSync(newRequest.completeUrl, "utf8");
-  let comments = loadComments();
-  let commentsList = comments.map(readCommentList);
-  guestPage = guestPage.replace("__COMMENTS__", commentsList.join("\n"));
-  return newResponse.generateGetResponse(newRequest.completeUrl, guestPage);
+const getCompleteUrl = function(url) {
+  let completeUrl = `${STATIC_DIR}${url}`;
+  if (url === "/") {
+    completeUrl = `${STATIC_DIR}/index.html`;
+  }
+  return completeUrl;
 };
 
 const loadResponseText = function(newRequest, newResponse) {
-  if (!fs.existsSync(newRequest.completeUrl)) {
-    return newResponse.data;
+  let completeUrl = getCompleteUrl(newRequest.url);
+  console.log(completeUrl);
+  if (!fs.existsSync(completeUrl)) {
+    newResponse.writeHead("404", "NOT FOUND");
+    newResponse.end();
   }
-  if (newRequest.hasMethodPost()) {
-    saveComments(newRequest);
-    return serveGuestPage(newRequest, newResponse);
+  if (newRequest.method === "POST") {
+    return saveComments(newRequest, newResponse);
   }
-  let newBody = fs.readFileSync(newRequest.completeUrl);
-  return newResponse.generateGetResponse(newRequest.completeUrl, newBody);
+  let newBody = fs.readFileSync(completeUrl);
+  generateGetResponse(completeUrl, newResponse, newBody);
 };
 
-const getHeadAndBody = function(headAndBody, line) {
-  if (line === "" && !headAndBody.body) {
-    headAndBody["body"] = "";
-    return headAndBody;
-  }
-  if ("body" in headAndBody) {
-    line = line.replace(/\+/g, " ");
-    line = decodeURIComponent(line);
-    headAndBody.body += line;
-    return headAndBody;
-  }
-  headAndBody.headers.push(line);
-  return headAndBody;
+const generateGetResponse = function(url, newResponse, body) {
+  const fileExt = getFileExtension(url);
+  const statusLine = { statusCode: "200", statusMsg: "OK" };
+  newResponse.setHeader("Content-Type", `text/${fileExt}`);
+  newResponse.writeHead(statusLine.statusCode, statusLine.statusMsg);
+  newResponse.write(body);
+  newResponse.end();
 };
 
-const generateRequestData = function(data) {
-  const [request, ...headersAndBody] = data.split("\r\n");
-  let [method, url, protocol] = request.split(" ");
-  const headAndBody = headersAndBody.reduce(getHeadAndBody, { headers: [] });
-  const req = { method, url, protocol };
-  const newRequest = new Request(req, headAndBody);
-  return newRequest;
-};
-
-const generatePageResponse = function(data) {
-  const newRequest = generateRequestData(data);
-  const newResponse = new Response(DEFAULT_RESPONSE, DEFAULT_HEADERS, "");
-  let url = newRequest.completeUrl;
-  if (newRequest.hasMethodGet() && url.includes("/public/guestBook.html")) {
-    return serveGuestPage(newRequest, newResponse);
-  }
-  return loadResponseText(newRequest, newResponse);
+const serveGuestPage = function(newRequest, newResponse) {
+  let completeUrl = getCompleteUrl(newRequest.url);
+  let guestPage = fs.readFileSync(completeUrl, "utf8");
+  let comments = loadOlderComments();
+  let commentsList = comments.map(readCommentList);
+  guestPage = guestPage.replace("__COMMENTS__", commentsList.join("\n"));
+  generateGetResponse(completeUrl, newResponse, guestPage);
 };
 
 const main = function(port) {
-  server = new Server();
-
-  server.on("connection", socket => {
-    console.warn("server is listening on:", socket.localPort);
-    socket.setEncoding("utf8");
-    socket.on("close", () => {
-      console.warn(`closing ${socket.remotePort}`);
-    });
-    socket.on("end", () => {
-      console.warn(`ending ${socket.remotePort}`);
-    });
-    socket.on("data", data => {
-      const pageResponse = generatePageResponse(data);
-      socket.write(pageResponse.head);
-      socket.write(pageResponse.body);
-    });
+  const server = http.createServer();
+  server.on("request", (req, res) => {
+    if (req.method === "GET" && req.url == "/guestBook.html") {
+      return serveGuestPage(req, res);
+    }
+    return loadResponseText(req, res);
   });
   server.listen(port);
 };
