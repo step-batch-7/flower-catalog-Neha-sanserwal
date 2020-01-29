fae0516d57ef996f0b2e3269a0e19791d5fcec04
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-01-29 18:06:58 +0530

    modified eslint

--- public/js/animateJar.js
+++ public/js/animateJar.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line no-unused-vars
 const hideJar = function() {
-  // eslint-disable-next-line no-undef
   const jar = document.getElementById('waterJar');
   jar.style.visibility = 'hidden';
   const timeOut = 1000;
